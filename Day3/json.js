0ab2e190d427879b8a100aecf411207c5864274d
Vaibhav Vashishtha <vaibhavvashishtha@Administrators-MacBook-Air.local>
2020-05-22 15:54:00 +0530

    Json update

--- Day3/json.js
+++ Day3/json.js
@@ -1,7 +1,23 @@
 var fs = require('fs');
 
+//byte array from file
 var buf = fs.readFileSync('json.json');
+
+//byte array to string
 var data = buf.toString();
+
+//format string to JSON
 var dataJSON = JSON.parse(data);
-console.log(data);
-console.log(dataJSON);
+
+//log in console
+if (dataJSON.firstname != '') {
+    console.log(dataJSON.firstname);
+}
+
+//Update json and write it back
+if (dataJSON.age > 30) {
+    dataJSON.age = 18;
+    var buf1 = JSON.stringify(dataJSON);
+    fs.writeFileSync('json.json', buf1);
+    console.log('no still 18 :) ' + dataJSON.age);
+}
