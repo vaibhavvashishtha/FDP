a90a40bcb12c530e1f2803fb9b1c05ac2c0f77ec
Vaibhav Vashishtha <vaibhavvashishtha@Administrators-MacBook-Air.local>
2020-05-22 14:19:54 +0530

    updated code

--- Day3/7.js
+++ Day3/7.js
@@ -1,2 +1,25 @@
+var http = require('http');
+
 //command line arguments
 console.log(process.argv[2]);
+
+//if argv[2] is hello then say hello Vaibhav
+http.createServer(function(req,res) {
+    if (process.argv[2] == 'hello') {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write('hello ' + process.argv[3]);
+        res.end;
+    } else if (process.argv[2] == 'hi') {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write('hi ' + process.argv[3]);
+        res.end;
+    } else {
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        res.write('hi ' + process.argv[3]);
+        res.end;
+    }
+}).listen(3000);
+
+function createResponse() {
+    
+}
