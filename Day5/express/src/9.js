58edea03d6732f25db0a06e0d4df36d8526792cb
Vaibhav Vashishtha <vaibhavvashishtha@Administrators-MacBook-Air.local>
2020-05-25 12:29:31 +0530

    Pre Partials

--- Day5/express/src/9.js
+++ Day5/express/src/9.js
@@ -1,31 +1,32 @@
+//To Understand Partials
 var path = require('path');
 var express = require('express');
 
 var app = express();
 var publicdir = path.join(__dirname, '../public/');
-//var publicviewsdir = path.join(__dirname, '../public/views/');
+var templatesdir = path.join(__dirname, '../templates/');
 
 app.set('view engine','hbs');
-//app.set('views', publicviewsdir);
+app.set('views', templatesdir);
 
 app.use(express.static(publicdir));
 
 app.get('', (req,res) => {
-    res.render('index', {
+    res.render('views/index', {
         title: 'Test Page',
         name: 'Vaibhav'
     });
 })
 
 app.get('/about', (req,res) => {
-    res.render('about', {
+    res.render('views/about', {
         title: 'About Page',
         imgsrc: '/img/google.png'
     });
 })
 
 app.get('/help', (req,res) => {
-    res.render('help', {
+    res.render('views/help', {
         title: 'Help Page'
     });
 })
